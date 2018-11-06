a813780df30d5c274d1e023b68c4e514e5d21ead
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-11-06 09:30:08 +0100

    Adding exercice 3 and boilerplate

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Exo 3
+
+
+function Cards(title, phrase, description) {
+
+  this.title = title;
+  this.phrase = phrase;
+  this.description = description;
+
+  this.getValue = function () {
+
+    title = document.getElementById("title").value;
+    phrase = document.getElementById("phrase").value;
+    description = document.getElementById("description").value;
+    this.createProducts(title, phrase, description);
+
+  }
+  
+  this.createProducts = function(title, phrase, description){
+
+    parentManager = document.getElementById('cardList');
+    childElement = document.createElement('div');
+    titleElement = document.createElement('h1');
+    phraseElement = document.createElement('p');
+    descriptionElement = document.createElement('p');
+    titleElement.innerHTML = title;
+    phraseElement.innerHTML = phrase;
+    descriptionElement.innerHTML = description;
+    parentManager.appendChild(childElement);
+    childElement.appendChild(titleElement);
+    childElement.appendChild(phraseElement);
+    childElement.appendChild(descriptionElement);
+
+    this.clearAllInput();
+  }
+
+  this.clearAllInput = function(title, phrase, description){
+    title = document.getElementById('title'). value = "";
+    phrase = document.getElementById('phrase').value = "";
+    description = document.getElementById('description').value = "";
+  }
+}
+
+
+
+
+let card = new Cards(title, phrase, description);
